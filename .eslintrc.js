89336fdcd336e09f6737c1b4c3beac2fd5199fdc
卫慧杰 <w920098695@sina.cn>
2022-06-20 11:18:25 +0800

    feat: git husky 初始化，eslit+prettierprettierrc 初始化搭建

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+	root: true,
+	env: {
+		node: true
+	},
+	extends: [
+		'plugin:vue/vue3-essential',
+		'eslint:recommended',
+		'@vue/typescript/recommended',
+		'@vue/prettier',
+	],
+	parserOptions: {
+		ecmaVersion: 2020
+	},
+	rules: {
+		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'@typescript-eslint/no-explicit-any': 'off',
+		'no-undef': 'off',
+		'@typescript-eslint/explicit-module-boundary-types': 'off',
+		'vue/no-mutating-props': 'off',
+		'@typescript-eslint/no-non-null-assertion': 'off'
+	}
+}
